7efa7b09ee27dab08090e37675c6ebd4e562693f
Chutikarn Mongkolthanachok <94603113+JaoTle@users.noreply.github.com>
2022-11-14 14:48:43 +0700

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,8 @@
 var baseURL = 'http://192.168.1.116:9000/';
-
+//url ต้องเป็น ipaddr ของinternet ดูได้จาก cmd -> ipconfig (IPv4 Address Wireless LAN หรือ Ethernet)
 Map<String,String> headers = {
     'Content-Type' : "application/json",
     'Content-Length' : "-1",
     'Host' : "192.168.1.116:9000"
+//url ต้องเป็น ipaddr ของinternet ดูได้จาก cmd -> ipconfig (IPv4 Address Wireless LAN หรือ Ethernet)
   };
